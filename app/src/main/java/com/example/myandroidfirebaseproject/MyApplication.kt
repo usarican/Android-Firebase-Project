5e94d976591e5dd7bcda6151e57af1702355c39b
ibrahimutkusarican <utku.sarican@rapsodo.com>
2023-11-07 10:30:24 +0300

    Added Hilt dependencies.

---
+++ app/src/main/java/com/example/myandroidfirebaseproject/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.myandroidfirebaseproject
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MyApplication : Application() {
+}
